4c7be4b2af7bb603d265b85adcd2fca692941208
suhwa <tnghk9611@naver.com>
2025-02-28 02:39:12 +0900

    feat: 자기소개 페이지 구현 완료

--- js/index.js
+++ js/index.js
@@ -1,19 +1,36 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
+// index.js
+document.addEventListener("DOMContentLoaded", function () {
+  // 댓글 등록 기능
+  const commentForm = document.querySelector(".comment-writing-container form");
+  const commentList = document.querySelector(".comment-list");
+  const commentTextarea = commentForm.querySelector("textarea");
+  const submitButton = commentForm.querySelector('button[type="button"]');
 
-console.log("알맞은 스크립트를 작성하세요");
+  submitButton.addEventListener("click", function () {
+    const commentText = commentTextarea.value.trim();
+
+    if (commentText) {
+      // 새 댓글 아이템 생성
+      const newComment = document.createElement("li");
+      newComment.innerHTML = `
+        <div class="comment-item">
+          <div class="comment-author">
+            <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+            <span>방문자</span>
+          </div>
+          <div class="comment-content">
+            ${commentText}
+          </div>
+        </div>
+      `;
+
+      // 댓글 목록의 맨 위에 새 댓글 추가
+      commentList.prepend(newComment);
+
+      // 입력 필드 초기화
+      commentTextarea.value = "";
+    } else {
+      alert("댓글 내용을 입력해주세요.");
+    }
+  });
+});
